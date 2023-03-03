7ec2e1dfc7be13972dcc53866eecb438e446014b
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2023-03-03 09:32:37 +0100

    Update for 2022/2023

---
+++ ej1/webapp/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var mysql = require('mysql');
+var app = express();
+
+app.get('/', function(req, res) {
+    var connection = mysql.createConnection({
+      host     : "xxx-aisi2223-db",
+      user     : "xxx-aisi2223",
+      password : "12345",
+      database : "xxx-aisi2223-database"
+    });
+
+    var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+    connection.connect(function(err) {
+	var msg = '<h2><u>GEI AISI 2022/2023: Node.js+Express+MariaDB</u>\n<p>URL: ' +url+ '\n<p> ' +new Date()+ '\n<p>MariaDB connection from user ' +connection.config.user+ ': ';
+
+        if(!err) {
+	    res.type('text/html').send(msg+ '<span style="color: green;">PASSED</span>\n');
+        } else {
+            res.type('text/html').send(msg+ '<span style="color: red;">FAILED</span>\n<p>'+err+'\n');
+        }
+        connection.end();
+    });
+});
+
+app.listen(80, function () {
+    console.log('Node.js app listening on port 80!');
+});
+
+
